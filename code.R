844b14e247795353e8579a8dd44d6fcc3cd5c7c0
Nafissah Pouye <Nafissah Pouye>
2019-07-24 10:25:19 +0000

    update vars

--- code.R
+++ code.R
@@ -7,11 +7,11 @@ setwd("C:/Users/LENOVO T46OS/Desktop/dra-immap-microdata1")
 data <- read_excel("resource1.xlsx", sheet = "Sheet1", skip = 2)
 
 #Xchange Bangladesh microdata
-selectedKeyVars <- c('Q104',	'Q11', 'Q13',	'Q14',	
+selectedKeyVars <- c('Q10',	'Q11', 'Q13',	'Q14',	
                      'Q15',	'Q16',	'Q17',	'Q18',
                      'Q19',	'Q20',	'Q21',	'Q22',
                      'Q23',	'Q24',	'Q25',	'Q26',
-                     'Q27','Q252'
+                     'Q27','Q256'
                      )
 #weightVar <- c('weight')
 
